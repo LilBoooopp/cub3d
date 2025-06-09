f7e1d95ed3c77f06f313a02b40b666d7f2e1a5bf
LilBoooopp <cbopp@student.42lausanne.ch>
2025-06-09 18:47:39 +0400

    fixed 5 param function

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 17:59:10 by cbopp             #+#    #+#             */
-/*   Updated: 2025/06/09 18:09:04 by cbopp            ###   ########.fr       */
+/*   Updated: 2025/06/09 18:45:11 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,22 @@ void	set_bg(t_img *back, t_cub *c)
 		set_vec2(WIN_WIDTH, WIN_HEIGHT / 2), c->tex.floor);
 }
 
-void	spawn_rays(t_cub *cub, t_img *back, int slice, pthread_t *threads,
-		t_thrdata *td)
+void	spawn_rays(t_spawn_args *args)
 {
 	int	i;
 
 	i = 0;
 	while (i < NUM_THREADS)
 	{
-		td[i].cub = cub;
-		td[i].img = back;
-		td[i].x_start = i * slice;
+		args->td[i].cub = args->cub;
+		args->td[i].img = args->back;
+		args->td[i].x_start = i * args->slice;
 		if (i == NUM_THREADS - 1)
-			td[i].x_end = WIN_WIDTH;
+			args->td[i].x_end = WIN_WIDTH;
 		else
-			td[i].x_end = (i + 1) * slice;
-		pthread_create(&threads[i + 1], NULL, render_thread, &td[i]);
+			args->td[i].x_end = (i + 1) * args->slice;
+		pthread_create(&args->threads[i + 1], NULL, render_thread,
+			&args->td[i]);
 		i++;
 	}
 }
@@ -76,11 +76,11 @@ void	*render_thread(void *arg)
 
 int	render(t_cub *cub)
 {
-	pthread_t	threads[NUM_THREADS + 1];
-	t_thrdata	td[NUM_THREADS];
-	t_mapdata	md;
-	t_img		back;
-	int			slice;
+	pthread_t		threads[NUM_THREADS + 1];
+	t_thrdata		td[NUM_THREADS];
+	t_mapdata		md;
+	t_img			back;
+	t_spawn_args	args;
 
 	if (cub->state == STATE_MENU)
 		return (draw_menu(cub), 0);
@@ -88,10 +88,11 @@ int	render(t_cub *cub)
 	md.cub = cub;
 	md.img = make_image(cub,
 			set_vec2(cub->map->screenx, cub->map->screeny), 0x00000000);
-	slice = WIN_WIDTH / NUM_THREADS;
+	args.slice = WIN_WIDTH / NUM_THREADS;
 	create_map_thread(cub, &threads[0], &md);
-	spawn_rays(cub, &back, slice, threads, td);
-	join_threads(threads, NUM_THREADS + 1);
+	args = setup_args(cub, &back, threads, td);
+	spawn_rays(&args);
+	join_threads(args.threads, NUM_THREADS + 1);
 	draw_image_transparent(&md.img, &back,
 		set_vec2((double)WIN_WIDTH - md.img.size.x, 0));
 	mlx_destroy_image(cub->mlx, md.img.img);
